557d87317ea36ccb59c0accb436aa5b0712868de
ToraKH <149379767+ToraKH@users.noreply.github.com>
2024-03-25 12:59:21 +0100

    Added all images, config file and manager file

---
+++ config.py
@@ -0,0 +1,26 @@
+
+# Screen size
+SCREEN_X = 1200
+SCREEN_Y = 600
+
+BORDER = 50
+
+# Sprite images
+SHIP_PLAYER1 = "spaceship_small_red.png"
+SHIP_PLAYER2 = "spaceship_small_blue.png"
+BACKGROUND = "cave.png"
+OBSTACLE = "alien.png"
+BULLET = "bullet.png"
+
+GRAVITY = 1
+FUELCOOLDOWN = 1    #Hvor lang tid det skal gå før ny fuel barrel kommer?????
+FUELLIMIT = 100       #Størrelsen på tanken
+FUELUSE = 50    #amount of fuel used
+
+
+HEALTH = 250          #Max health
+HITPOINT = -10
+SCOREPOINT = 10
+
+MOVEMENT = 5
+
